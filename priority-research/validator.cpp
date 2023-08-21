3d5f290b7c458cb1369846b048ba8f7d6d2562f3
RedBlazerFlame <protondecay314@gmail.com>
2023-08-21 16:29:50 +0800

    Changed int to long long in validator (priority-research)

--- priority-research/validator.cpp
+++ priority-research/validator.cpp
@@ -7,7 +7,7 @@ const ll max_n = 1'000'000'000'000'000'000ll;
 int main(int argc, char** argv) {
     registerValidation(argc, argv);
     
-    ll N = inf.readInt(1, max_n, "N");
+    ll N = inf.readLong(1, max_n, "N");
     inf.readEoln();
 
     return 0;
