30bc7e0fce846d1af371692b762b44dd97b660dd
RedBlazerFlame <protondecay314@gmail.com>
2023-08-23 21:48:19 +0800

    Added generator and validator (adtech-guide-oval-easy)

---
+++ adtech-guide-oval-easy/validator.cpp
@@ -0,0 +1,15 @@
+#include "testlib.h"
+using namespace std;
+typedef long long ll;
+
+const ll max_n = 5'000ll;
+
+int main(int argc, char** argv) {
+    registerValidation(argc, argv);
+    
+    ll N = inf.readLong(1, max_n, "N");
+    inf.readEoln();
+    inf.readEof();
+
+    return 0;
+}
